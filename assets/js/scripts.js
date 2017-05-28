8c61119266fe88862f75901ad669be1a902c87dc
Stacy Bridges <stacy.bridges@g.austincc.edu>
2017-05-27 21:32:42 -0500

    added watch and uglify tasks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,7 @@
 // scripts.js
+var one = 1,
+	two = 2,
+	three = 3,
+	four = 4;
+
+function test(){ return "howdy"; }
--- gruntfile.js
+++ gruntfile.js
@@ -15,6 +15,35 @@ module.exports = function(grunt) {
 		sass: {
 	        options: { sourceMap: false },
 	        dist: { files: { 'css/styles.css': 'assets/scss/styles.scss' } }
+	    },
+
+	    /**
+		* Grunt Contrib Uglify
+		* Minify JavaScript files
+		* https://www.npmjs.com/package/grunt-contrib-uglify
+	    */
+	    uglify: {
+	    	my_target: {
+	    		files: {
+	    			'js/scripts.js' : ['assets/js/scripts.js', 'node_modules/jquery/dist/jquery.js']
+	    		}
+	    	}
+	    },
+
+	    /**
+		* Grunt Contrib Watch
+		* Monitor files and execute tasks
+		* https://www.npmjs.com/package/grunt-contrib-watch
+	    */
+	    watch: {
+		  	sass: {
+		    	files: ['assets/scss/*.scss'],
+		    	tasks: ['sass']
+		    },   	
+		  	scripts: {
+		    	files: ['assets/js/*.js'],
+		    	tasks: ['uglify']
+		    }  		    
 	    }
 	});
 
@@ -22,7 +51,7 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt); // loads 'grunt-'' plugins from package.json
 
 	// default task
-	grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['watch']);
 
 	// custom tasks
 
